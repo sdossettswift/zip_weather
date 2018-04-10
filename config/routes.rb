0020eb68222590754028d854400ba0c07f6ab9aa
S. Dossett Swift <sdossettswift@gmail.com>
2018-04-10 14:35:26 -0500

    rails routes only

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :entries
-
-  post 'locate' => 'weather#locate', as: :locate
-  root 'weather#index'
+  resources :entries, only: [:new, :index, :create ]
+  root 'entries#index'
+  #post 'locate' => 'weather#locate', as: :locate
+  #root 'weather#index'
 
 end
