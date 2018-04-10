ab191f512248c86457e56aae944279cd087a33c3
S. Dossett Swift <sdossettswift@gmail.com>
2018-04-10 01:53:55 -0500

    added regex validation for 5 digit zip

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
   scope :five_latest, -> { order(created_at: :desc).limit(5) }
   validates_length_of :zip, :is => 5
-  #TODO: add validation -> zip code length(5); regex(5digits) (per specs )
+  validates_format_of :zip, :with => /\d{5}/
 end
